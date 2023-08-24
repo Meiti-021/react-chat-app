84ed9b3d58fa58cfc550e2ebffcfbc351be553ac
Mahdi <meiti.edu@gmail.com>
2023-08-25 00:31:55 +0430

    create chat media component

--- src/components/ChatMedia.tsx
+++ src/components/ChatMedia.tsx
@@ -1,5 +1,103 @@
+import * as React from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { Dialog, DialogTitle } from "@mui/material";
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+  },
+];
 const ChatMedia = () => {
-  return <div>hi</div>;
+  const [open, setOpen] = React.useState(false);
+  const [imageIndex, setImageIndex] = React.useState<number>(0);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <ImageList
+        sx={{ width: "100%", height: "auto", mt: "-0.05rem" }}
+        cols={3}
+        rowHeight={130}
+      >
+        {itemData.map((item, index) => (
+          <ImageListItem key={item.img}>
+            <img
+              src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+              onClick={() => {
+                setOpen(true);
+                setImageIndex(index);
+              }}
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {itemData[imageIndex].title}
+        </DialogTitle>
+        <img
+          src={`${itemData[imageIndex].img}?w=164&h=164&fit=crop&auto=format`}
+          alt={itemData[imageIndex].title}
+          style={{ width: "500px", height: "auto", objectFit: "contain" }}
+        />
+      </Dialog>
+    </>
+  );
 };
 
 export default ChatMedia;
